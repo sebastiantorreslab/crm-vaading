e9002e1ae61d68ceda4f864b60a5da1182b66009
sebastiantorreslab <str9371@gmail.com>
2024-04-19 22:55:26 -0500

    crm

--- src/main/java/com/example/application/data/ContactRepository.java
+++ src/main/java/com/example/application/data/ContactRepository.java
@@ -2,7 +2,19 @@ package com.example.application.data;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    @Query("SELECT c FROM Contact c " + "WHERE LOWER(c.firstName ) LIKE LOWER(CONCAT('%', :searchTerm, '%'))" +
+            "OR LOWER (c.lastName) LIKE LOWER (CONCAT('%',:searchTerm,'%'))")
+    List<Contact> search(@Param("searchTerm") String searchTerm);
+
+
+
+
 
 }
